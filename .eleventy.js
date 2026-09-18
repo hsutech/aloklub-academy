6b080f03eab415b8eebf725b9d2dac94dfc08ead
Erik Hsu <erikhsu@Eriks-MacBook-Pro.local>
2026-09-18 11:59:20 +0200

    Preserve current production pages and routing in automatic builds

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,15 @@ export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/_redirects": "_redirects" });
   eleventyConfig.addPassthroughCopy({ "src/sitemap.xml": "sitemap.xml" });
 
+  eleventyConfig.addPassthroughCopy({ "src/_headers": "_headers" });
+
+  eleventyConfig.addPassthroughCopy({ "src/favicon.svg": "favicon.svg" });
+
+  eleventyConfig.addPassthroughCopy({ "src/favicon.ico": "favicon.ico" });
+
+  eleventyConfig.ignores.add("src/404.html");
+  eleventyConfig.addPassthroughCopy({ "src/404.html": "404.html" });
+
   return {
     dir: {
       input: "src",
